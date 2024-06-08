78faab94b9766765fd078e807e1bdb586e1e2d92
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2024-06-08 23:42:35 +0300

    remove ui dep

--- nest.js
+++ nest.js
@@ -8,7 +8,7 @@ function delay(time) {
 }
 
 async function goNest(dirPath, filePath) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
 
     await page.setViewport({width: 1920, height: 1080})

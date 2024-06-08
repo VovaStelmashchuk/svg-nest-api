f259a3a7241ac250f39ec617bd41ad347e4c57e4
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2024-06-09 00:18:26 +0300

    Use origin svg nest

--- nest.js
+++ nest.js
@@ -15,7 +15,7 @@ async function goNest(dirPath, filePath) {
     const page = await browser.newPage();
 
     await page.setViewport({width: 1920, height: 1080})
-    await page.goto('http://localhost:5001');
+    await page.goto('https://svgnest.com/');
 
     const fileInput = await page.$('input[type="file"]');
     await fileInput.uploadFile(filePath);

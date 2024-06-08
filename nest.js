f19525ba428531672ecc22d023ffea94c4f91003
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2024-06-08 23:49:29 +0300

    add --no-sand-box

--- nest.js
+++ nest.js
@@ -8,7 +8,10 @@ function delay(time) {
 }
 
 async function goNest(dirPath, filePath) {
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
 
     await page.setViewport({width: 1920, height: 1080})
